0819b0b13f931d489354dca53f7533f4f36b7540
Jean Carlos Corrêa Couto <jean.couto@consulting-for.edenred.com>
2021-03-30 15:04:55 -0300

    feat(email): envio de email

--- src/app/shared/service/email.service.ts
+++ src/app/shared/service/email.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IMensagem } from '../model/mensagem';
+declare let Email: any;
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,16 @@ export class EmailService {
   constructor() { }
 
   public enviarEmail(mensagem: IMensagem): void {
-    console.log(mensagem);
+    Email.send({
+      Host : 'smtp.gmail.com',
+      Username : '<EMAIL_GMAIL>',
+      Password : '<SENHA_GMAIL>',
+      To : 'jcorrearafiapacking@gmail.com',
+      From : mensagem.email,
+      Subject : mensagem.titulo,
+      Body : mensagem.mensagem
+    }).then(
+      alert('Seu email foi enviado. Logo entraremos em contato. Obrigado!')
+    );
   }
-
 }
